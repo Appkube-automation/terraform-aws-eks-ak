20048254cb8ea7aac61cf4e7ddc0bc0e423ea3a1
synectiks-code <papu.bhattacharya@synectiks.com>
2021-09-25 22:45:53 +0530

    remove kube tags

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -22,17 +22,4 @@ module "vpc" {
   enable_nat_gateway   = var.enable_nat_gateway
   single_nat_gateway   = var.single_nat_gateway
 
-  tags = {
-    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-  }
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                    = "1"
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"           = "1"
-  }
 }
